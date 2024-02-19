ad5826107b053175c918b659b37bc763ba7dc14d
Will Dalgleish <dev@wdalgleish.com>
2024-02-19 11:51:01 +0000

    feat: iteration repeatCount is specified as an input

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,16 +3,40 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("repeat 'a' 5 times", func(t *testing.T) {
+		got := Repeat("a", 5)
+		want := "aaaaa"
+		assertCorrectMessage(t, got, want)
+	})
 
-	if repeated != expected {
-		t.Errorf("expect %q got %q", expected, repeated)
+	t.Run("repeat 'hello' 3 times", func(t *testing.T) {
+		got := Repeat("hello", 3)
+		want := "hellohellohello"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("repeat 'a' 1 times", func(t *testing.T) {
+		got := Repeat("a", 1)
+		want := "a"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("repeat 'a' 1 times", func(t *testing.T) {
+		got := Repeat("a", 0)
+		want := ""
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
 	}
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
